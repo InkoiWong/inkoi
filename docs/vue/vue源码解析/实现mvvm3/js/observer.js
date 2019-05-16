43ef67641ccffdeafb677eb2fb8ffc8bd7781124
韵楷 <1103504177@qq.com>
2019-05-16 14:00:45 +0800

    update

---
+++ docs/vue/vue源码解析/实现mvvm3/js/observer.js
@@ -0,0 +1,108 @@
+/**
+ * @desc  创建监听者（观察者）
+ *        劫持监听所有属性
+ *
+ *        P.S 暂时不讨论对data递归的情况
+ */
+class Observer {
+  /**
+   *Creates an instance of Observer.
+   * @param {Object} data 传入的就是每个vue里面的data
+   */
+  constructor(data) {
+    this.observe(data);
+  }
+
+  /**
+   * @desc  进行监听动作
+   *        解析数据，完成对数据属性的劫持，控制对象属性的get和set
+   *
+   *        可被递归使用
+   *
+   * @param {Object} data 传入的vue的data 或 其子属性（递归情况下）
+   */
+  observe(data) {
+    // 判断数据的有效性（类型必须是对象）
+    if (!data || typeof data !== 'object') {
+      return;
+    } else {
+      // 遍历该对象的所有属性
+      var keys = Object.keys(data);
+      keys.forEach(key => {
+        this.defineReactive(data, key, data[key]);
+      });
+    }
+  }
+  /**
+   * @desc  包装器（可观测化）
+   *        将数据包装成一种可观测的类型，当数据产生变更的时候，我们能够感知到
+   *
+   * @param {*} obj 传入的vue的data
+   * @param {*} key 被遍历到的data中的属性
+   * @param {*} val data中该属性的值
+   */
+  defineReactive(obj, key, val) {
+    // 每一个 reactive data 的创建，都会随着创建一个 `dep 实例`
+    var dep = new Dep();
+
+    // var childObj = observe(val);
+
+    Object.defineProperty(obj, key, {
+      // 是否可遍历
+      enumerable: true,
+      // 是否可配置
+      configurable: true,
+      // 取值方法
+      get() {
+        // 每次 new 一个 watcher（订阅者）对象的时候需要计算依赖的dep对象，
+        // Dep.target 就是当前正在计算依赖的watcher对象
+
+        var curHandlingWatcher = Dep.target;
+        if (curHandlingWatcher) {
+          // 如果有值，证明这是由 Watcher初始化 的时候调用的get方法
+
+          dep.addSub(curHandlingWatcher);
+        }
+
+        return val;
+      },
+      // 修改值
+      set(newValue) {
+        val = newValue;
+
+        dep.notify();
+      }
+    });
+  }
+}
+
+/**
+ * @desc  订阅者管理器
+ *        用于储存 订阅者依赖：addSub
+ *        和集体发布更新：notify
+ */
+class Dep {
+  constructor() {
+    // 用于储存所有 订阅者依赖 的数组
+    this.subs = [];
+  }
+
+  /**
+   * @desc  添加订阅
+   *        每新建一个Watcher，就往管理器中添加一个依赖
+   *
+   * @param sub 其实就是watcher对象
+   */
+  addSub(sub) {
+    this.subs.push(sub);
+  }
+  /**
+   * @desc  集体发布更新
+   *        更新所有在管理器中添加过依赖的watcher
+   */
+  notify() {
+    this.subs.forEach(sub => {
+      sub.update();
+    });
+  }
+}
---
+++ docs/vue/vue源码解析/实现mvvm3/js/watcher.js
@@ -0,0 +1,53 @@
+/**
+ * @desc  创建订阅者
+ */
+class Watcher {
+  /**
+   *Creates an instance of Watcher.
+   * @param {*} vm    全局vm对象
+   * @param {*} expr  订阅的节点
+   *                  v-model、v-text、:value、{{}}等方法所关联的节点
+   * @param {*} cb    发布更新时需要做的回调函数
+   *                  在 Dep 的 notify 方法中被触发
+   */
+  constructor(vm, expr, cb) {
+    // 缓存重要属性
+    this.vm = vm;
+    this.expr = expr;
+    this.cb = cb;
+
+    // 将自己添加到Dep订阅器的操作
+    this.value = this.get();
+  }
+
+  get() {
+    // Dep.target 是一个在全局缓存的量
+    // 用于证明现在正在处理计算依赖的 Watcher 是自己
+    // 当处理完毕（即已添加进Dep管理器中）后，再将其去掉
+
+    // 缓存自己
+    Dep.target = this;
+    // 通过'获取当前值'强制执行 Observer监听器 里的get函数
+    var value = this.vm.$data[this.expr];
+    // 释放全局中的缓存
+    Dep.target = null;
+
+    return value;
+  }
+  /**
+   * @desc      发布更新时需要做的事情
+   *            当有修改时才执行
+   *
+   * @important 所以这里就是实现vue只更新被修改的数据节点的关键
+   */
+  update() {
+    var newValue = this.vm.$data[this.expr];
+    var oldValue = this.value;
+
+    // 当判断有更改
+    if (oldValue !== newValue) {
+      // 执行回调
+      this.cb(newValue);
+    }
+  }
+}
